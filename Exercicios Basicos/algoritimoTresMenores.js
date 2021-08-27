f7136fedb03e4cac58345c465bb3635861c345c0
Biamah <beatriz.souza.costa@outlook.com>
2021-08-26 21:51:49 -0300

    exercicio tres menores pronto

--- Exercicios Basicos/algoritimoTresMenores.js
+++ Exercicios Basicos/algoritimoTresMenores.js
@@ -1,2 +1,13 @@
 // Faça um algoritmo que leia 3 números
 // inteiros e mostre o menor deles.
+
+let numeros = [50, 25, 300];
+
+let numero = numeros[0];
+for (const i of numeros) {
+  if (i < numero) {
+    numero = i
+  }
+}
+
+console.log(numero);
