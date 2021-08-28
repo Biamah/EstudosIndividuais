ecdb62cda13731df16613c68463545c7f99cc25f
Biamah <beatriz.souza.costa@outlook.com>
2021-08-28 09:35:12 -0300

    exercicios basicos finalizados

--- Exercicios Basicos/algoritimoSalario.js
+++ Exercicios Basicos/algoritimoSalario.js
@@ -3,3 +3,31 @@
 // A empresa paga R$ 12 por hora e R$ 40 por dependentes. 
 // Sobre o salário são feito descontos de 8,5% para o INSS e 5% para IR. 
 // Faça um algoritmo para ler o Nome, número de horas trabalhadas e número de dependentes de um funcionário. Após a leitura, escreva qual o Nome, salário bruto, os valores descontados para cada tipo de imposto e finalmente qual o salário líquido do funcionário. 
+
+// calculo do salario Bruto
+function calculaSalarioBruto(horasTrabalhadas, dependentes) {
+  let calculaHoras = horasTrabalhadas * 12;
+  let calculaDependentes = dependentes * 40;
+  let calculo = calculaHoras + calculaDependentes;
+
+  return calculo;
+}
+
+console.log(calculaSalarioBruto(40, 3));
+
+// calculo do Desconto
+function calculoDesconto() {
+  let salario = calculaSalarioBruto(40, 3);
+  let descontoInss = salario * 0.085;
+  let descontoIr = salario * 0.05;
+  let calculo = descontoInss + descontoIr;
+
+  return calculo;
+}
+
+console.log(calculoDesconto());
+
+// salario Liquido
+const calculoSalarioLiquido = calculaSalarioBruto(40, 3) - calculoDesconto();
+
+console.log(calculoSalarioLiquido);
---
+++ Exercicios Basicos/exercicioDavid.js
@@ -0,0 +1,7 @@
+// Ordene um array de números, exemplo do array [3, 5, 2, 1, 4]
+let arrayDavid = [3, 5, 2, 1, 4];
+let ordena = arrayDavid.sort();
+
+console.log(ordena);
+
+// versão do David
