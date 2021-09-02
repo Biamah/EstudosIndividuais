72f42ce243bad58b7e391b805f31cb65142cb5a0
Biamah <beatriz.souza.costa@outlook.com>
2021-09-01 22:41:37 -0300

    subindo arquivos

--- Jogo Tentativa 1/JS/main.js
+++ Jogo Tentativa 1/JS/main.js
@@ -6,10 +6,19 @@ kaboom({
 // carregamentos
 loadSprite("flappy", "assets/sprites/flappy.png");
 loadSprite("bg", "assets/sprites/background.png");
-loadSprite("pipe", "assets/sprites/pipe.png")
+loadSprite("pipe", "assets/sprites/pipe.png");
+loadSprite("pipe-reverse", "assets/sprites/pipe-reverse.png");
 loadSound("wooosh", "assets/sounds/Suction-Cup.mp3");
+loadSound("miss", "assets/sounds/Wobble.mp3");
 
 scene("main", () => {
+
+  // const PIPE_MARGIM = 40;
+  // const PIPE_OPEN = 120;
+  const JUMP_FORCE = 320;
+  const SPEED = 120;
+
+  gravity(1200);
 
   // background
   add([
@@ -25,37 +34,80 @@ scene("main", () => {
     sprite("flappy"),
     pos(80, 80),
     body(),
-    // scale(3.5),
+    scale(0.7),
+  ]);
+
+  let score = 0;
+
+  const scoreLabel = add([
+    text(score, 32),
+    pos(12, 12),
   ]);
 
   // plataforma
   add([
     rect(width(), 20),
     pos(0, height() - 40),
-    solid(),
+    // solid(),
   ]);
 
   // pipe
-  const pipe = add([
-    sprite("pipe"),
-    pos(width(), 292),
-    scale(3),
-    // area(),
-    "pipe"
-  ]);
+  loop(2, () => {
+    add([
+      sprite("pipe"),
+      pos(width(), rand(500, 270)),
+      scale(3),
+      "pipe",
+      { passed: false, }
+    ]);
+  });
+
+  // pipe-reverse
+  loop(2, () => {
+    add([
+      sprite("pipe-reverse"),
+      pos(width(), rand(50, -150)),
+      scale(3),
+      "pipe-reverse"
+    ]);
+  });
 
   keyPress("space", () => {
-    player.jump();
+    player.jump(JUMP_FORCE);
     play("wooosh");
   });
 
-  pipe.action(() => {
-    pipe.move(-60, 0)
+  action("pipe", (pipe) => {
+    pipe.move(-SPEED, 0);
+
+    if (pipe.passed === false && pipe.pos.x < player.pos.x) {
+      pipe.passed = true;
+      score += 1;
+      scoreLabel.text = score;
+    }
+
+  });
+
+  action("pipe-reverse", (pipeReverse) => {
+    pipeReverse.move(-SPEED, 0);
   });
 
   player.collides("pipe", () => {
     go("lose");
-  })
+    play("miss");
+  });
+
+  player.collides("pipe-reverse", () => {
+    go("lose");
+    play("miss");
+  });
+
+  player.action(() => {
+    if (player.pos.y > height()) {
+      go("lose");
+      play("miss");
+    }
+  });
 });
 
 scene("lose", () => {
